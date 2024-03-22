54f764fde1dab540b4f8ada5b556a25d683423e2
Matsuel <matsuel0308@gmail.com>
2024-03-22 09:11:54 +0100

    add font family

--- src/Components/Hero/Hero.tsx
+++ src/Components/Hero/Hero.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import styles from './Hero.module.scss';
+import Logo from '@/assets/logo-high.png';
+import Image from 'next/image';
 
 interface HeroProps {
 
@@ -9,8 +10,14 @@ interface HeroProps {
 const Hero = ({ }: HeroProps) => {
     return (
         <div className={styles.Hero_container}>
-            cc
-            
+            <div className={styles.Hero_content}>
+                <h1 className={styles.Hero_title}>Matheo Lang</h1>
+                <h2 className={styles.Hero_subtitle}>I am a full stack web and mobile developer based in France. I love back-end development and I'm always looking for new challenges like doing css.</h2>
+            </div>
+            <div className={styles.Hero_image}>
+                <Image src={Logo} alt="Logo" />
+            </div>
+
         </div>
     );
 };
