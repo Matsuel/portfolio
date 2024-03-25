968d69445c1c3d9ab5b589e12d1adaa4a642364d
Matsuel <matsuel0308@gmail.com>
2024-03-25 16:08:22 +0100

    projects responsive

--- src/Sections/Projects/Projects.tsx
+++ src/Sections/Projects/Projects.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 import styles from './Projects.module.scss';
 import Title from '../../Components/Title/Title';
