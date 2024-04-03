2653b522805f935cc921103b385d95dce40c07e6
Matsuel <matsuel0308@gmail.com>
2024-04-03 13:29:27 +0200

    style for work section

--- src/Sections/Work/Work.tsx
+++ src/Sections/Work/Work.tsx
@@ -4,6 +4,17 @@ import styles from './Work.module.scss';
 import Title from '@/Components/Title/Title';
 import { Emoji } from 'emoji-picker-react';
 import Link from 'next/link';
+import useSWR from 'swr';
+
+const fetcher = (url: string) => {
+    return fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `token ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+        },
+    }).then((response) => response.json());
+}
 
 type WorkType = {
     title: string;
@@ -11,7 +22,7 @@ type WorkType = {
     link: string;
 }
 
-const Works : WorkType[] = [
+const Works: WorkType[] = [
     {
         title: "Portfolio",
         description: "This is my portfolio website create with Next.js and SCSS in order to show my work and my skills",
@@ -26,9 +37,14 @@ const Works : WorkType[] = [
 
 const Work = () => {
 
+    // const {data, error} =useSWR('https://api.github.com/users/Matsuel/repos', fetcher);
+    // if (error) return <div>failed to load</div>
+    // if (!data) return <div>loading...</div>
+    // console.log(data.filter((repo: any) => repo.homepage !== null));
+
     return (
         <section className={styles.Work_container} id="work">
-            <div className={styles.Work_top}>
+            <div className={styles.Work_topContainer}>
                 <Title content="My Work" />
                 <Emoji unified="1f3a8" size={70} />
             </div>
@@ -36,10 +52,30 @@ const Work = () => {
             <div className={styles.Works}>
                 {Works.map((work, index) => (
                     <div className={styles.Work} key={index}>
-                        <h3 className={styles.Work_title}>{work.title}</h3>
-                        <p className={styles.Work_description}>{work.description}</p>
-                        <Link href={work.link} passHref target='_blank'>
-                            View Project
+                        <div className={styles.Work_top}>
+                            <div className={styles.Work_names}>
+                                <h3 className={styles.Work_name}>
+                                    {work.title}
+                                </h3>
+                                <h4 className={styles.Work_fullname}>
+                                    @Matsuel
+                                </h4>
+                            </div>
+                            <div className={styles.Work_status}>
+                                Online
+                            </div>
+                        </div>
+
+                        <p className={styles.Work_description}>
+                            {work.description}
+                        </p>
+                        <div className={styles.Work_technologies}>
+                            <span className={styles.Work_technologie}>#Next.js</span>
+                            <span className={styles.Work_technologie}>#SCSS</span>
+                        </div>
+
+                        <Link href={work.link} passHref target='_blank' className={styles.Work_link}>
+                            View Project <Emoji unified="1f310" size={20} />
                         </Link>
                     </div>
                 ))}
