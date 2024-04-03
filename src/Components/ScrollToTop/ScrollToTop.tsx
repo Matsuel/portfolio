9959fc73eafc94eda7ce98df0599d07ea0abb3df
Matsuel <matsuel0308@gmail.com>
2024-04-03 15:19:39 +0200

    patch svg problems

--- src/Components/ScrollToTop/ScrollToTop.tsx
+++ src/Components/ScrollToTop/ScrollToTop.tsx
@@ -31,7 +31,7 @@ const ScrollToTop = () => {
     };
 
     return (
-        <div className={styles.ScrollToTop_link + (isFadingOut ? ' ' + styles.ScrollToTop_fadeOut : '')} onClick={goToTop} role='contentinfo'>
+        <div className={styles.ScrollToTop_link + (isFadingOut ? ' ' + styles.ScrollToTop_fadeOut : '')} onClick={goToTop} role='button' tabIndex={0}>
             <FontAwesomeIcon icon={faArrowUp} className={styles.ScrollToTop_icon} />
         </div>
     );
