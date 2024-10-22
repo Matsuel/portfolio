cb340937b04f45d413efa9ef923348649c743528
Matsuel <matsuel0308@gmail.com>
2024-10-22 19:52:32 +0200

    Compoentns BlurIn -> Add blurin title, edit colors of text selector

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import BlurIn from "@/components/BlurIn";
 import Navbar from "@/components/Navbar";
 import Title from "@/components/Title";
 import Utils from "@/components/Utils";
@@ -9,13 +10,15 @@ export default function Home() {
 
   return (
     <div
-      className={`flex flex-col items-center justify-start min-h-screen h-[150vh] p-8 pb-20 gap-16 bg-background relative ${isMouseLeave ? "opacity-20" : "opacity-100"} transition-opacity duration-500 ease-in-out`}
+      className={`flex flex-col items-center justify-start min-h-screen h-[150vh] p-8 pb-20 gap-16 bg-background relative ${isMouseLeave ? "opacity-20" : "opacity-100"} transition-opacity duration-700 ease-in-out`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       <Title title="Matheo Lang" />
       {/* <Cursor /> */}
       <Navbar />
+
+      <BlurIn word="Matheo Lang" />
 
 
       <Utils />
