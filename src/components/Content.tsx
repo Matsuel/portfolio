6aae272b58fa99a806453b0580d83a94c5fd0607
Matsuel <matsuel0308@gmail.com>
2024-12-18 21:39:58 +0100

    Content -> Add px

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -15,7 +15,7 @@ const Content = () => {
             className={`w-full flex flex-col items-center justify-start p-8 pt-24 gap-20 transition-opacity duration-700 ease-in-out`}
         >
             <Island />
-            <div className="fixed z-40 w-52 h-12 top-8 bg-black rounded-3xl flex flex-row justify-between">
+            <div className="fixed z-40 w-56 h-12 top-8 bg-black rounded-3xl flex flex-row justify-between px-2">
                 <div />
                 <CircleProgress percentage={scrollPercentage} />
             </div>
