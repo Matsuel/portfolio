915bb48861e47bd22bf9088a87d86366d753bbeb
Matsuel <matsuel0308@gmail.com>
2024-12-21 16:17:06 +0100

    Component Loader -> Create Loader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Content from "@/components/Content";
 
 export default function Home() {
+
   return (
     <div className="w-full bg-background flex min-h-screen h-[200vh] p-8 pb-20 relative">
       <Content />
