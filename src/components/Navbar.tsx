bc6b36efc2242da7b35d75eca6c678f5586ed623
Matsuel <matsuel0308@gmail.com>
2024-12-26 16:44:19 +0100

    Components -> Add assets navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react'
+import useSection from '@/hooks/useSection';
+import User from '@/assets/User';
 
 const Navbar = () => {
     const [isHovered, setIsHovered] = useState<boolean>(false);
+    const { section } = useSection();
 
 
     return (
-        <div className={`fixed top-8 left-[50%] transform -translate-x-1/2 z-50 w-[500px] flex justify-center items-start ${isHovered ? "bg-black rounded-xl h-52" : "h-auto"}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+        <div className={`fixed top-8 left-[50%] transform -translate-x-1/2 z-50 w-[500px] flex justify-center items-start h-auto ${isHovered ? "bg-black rounded-xl" : ""}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
             <div className='w-auto h-auto flex items-center justify-center gap-4'>
                 <div className='w-40 h-12 bg-black rounded-full' />
-                <div className='w-10 h-10 bg-black rounded-xl flex items-center justify-center' />
+                <div className='w-10 h-10 bg-black rounded-xl flex items-center justify-center text-text'>
+                    <User size={40} />
+                </div>
             </div>
 
         </div>
