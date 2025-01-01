c9e7d59faedaa8604838fc41ecb418c4b331f3f9
Matsuel <matsuel0308@gmail.com>
2025-01-01 15:54:11 +0100

    Component Hero -> Ajout titre

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,6 +6,13 @@ const Hero = () => {
             <p className='w-[30%] text-4xl font-bold text-text'>
                 Développeur web <span className='text-secondary'>spécialisé dans la création de </span> solutions simples <span className='text-secondary'>mais</span> efficaces, <span className='text-secondary'>avec une approche qui </span> redéfinit les standards du design
             </p>
+
+            <div className='w-auto h-auto flex flex-col gap-0'>
+                <h1 className='text-[200px] leading-none font-bold text-text uppercase'>Matheo</h1>
+                <div className='w-auto h-auto flex flex-row gap-4'>
+                    <h1 className='text-[200px] leading-none font-bold text-text uppercase'>Lang</h1>
+                </div>
+            </div>
         </section>
     )
 }
