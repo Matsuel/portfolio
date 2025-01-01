03105c247ec5ac4a60996e50b9ddbbb6687f7e22
Matsuel <matsuel0308@gmail.com>
2025-01-01 15:50:54 +0100

    Component Hero -> Ajout paragraphe

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import Theme from "@/components/Theme";
 import CircleProgress from './CircleProgress';
 import Hour from './Hour';
-import Navbar from './Navbar';
 import Hero from '@/components/Hero';
 
 const Content = () => {
@@ -12,13 +11,11 @@ const Content = () => {
 
     return (
         <div
-            className={`w-full flex flex-col items-center justify-start p-8 pt-24 gap-20 transition-opacity duration-700 ease-in-out`}
+            className={`w-full flex flex-col items-center justify-start p-8 gap-20 transition-opacity duration-700 ease-in-out`}
         >
-            <Navbar />
             <Theme />
             <CircleProgress />
             <Hour />
-
             <Hero />
         </div>
     )
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
-import HeroTitle from '@/components/HeroTitle'
 import React from 'react'
 
 const Hero = () => {
     return (
-        <section id='home' className='w-[83%] h-auto flex flex-col gap-8 bg-white justify-center items-center'>
-            <HeroTitle />
-
+        <section id='home' className='w-full h-auto flex flex-col gap-8 p-8 justify-start items-start relative'>
+            <p className='w-[30%] text-4xl font-bold text-text'>
+                Développeur web <span className='text-secondary'>spécialisé dans la création de </span> solutions simples <span className='text-secondary'>mais</span> efficaces, <span className='text-secondary'>avec une approche qui </span> redéfinit les standards du design
+            </p>
         </section>
     )
 }
