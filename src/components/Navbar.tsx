8764b47e637f6e949615011236ccb84371736328
Matsuel <matsuel0308@gmail.com>
2025-01-07 12:20:01 +0100

    Component Navbar -> Add styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, { useState } from 'react';
-import MenuIcon from './MenuIcon';
-import { AnimatePresence, motion } from 'framer-motion';
-import { navbarLinks } from '@/constantes/navbar';
+import React, { useState } from "react";
+import MenuIcon from "./MenuIcon";
+import { AnimatePresence, motion } from "framer-motion";
+import { navbarLinks } from "@/constantes/navbar";
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -18,19 +18,27 @@ const Navbar = () => {
             <AnimatePresence>
                 {isMenuOpen && (
                     <motion.div
-                        className="fixed w-full min-h-screen h-screen bg-black z-40 top-0 left-0"
-                        initial={{ y: "100%" }} // Commence en bas de l'écran
-                        animate={{ y: 0 }} // Se déplace vers le haut
-                        exit={{ y: "100%" }} // Retourne vers le bas
-                        transition={{ duration: 0.5, ease: "easeInOut" }} // Animation fluide
+                        className="fixed w-full min-h-screen h-screen bg-background flex flex-col justify-center items-center gap-5 z-40 top-0 left-0"
+                        initial={{ y: "100%" }}
+                        animate={{ y: 0 }}
+                        exit={{ y: "100%" }}
+                        transition={{ duration: 0.5, ease: "easeInOut" }}
                     >
-                        {navbarLinks.map((link) => (
-                            <button
+                        {navbarLinks.map((link, index) => (
+                            <motion.button
                                 key={link.name}
-                                className="w-full h-20 flex items-center justify-center text-white text-2xl"
+                                className="w-full h-auto flex flex-row items-start justify-center text-text"
+                                onClick={toggleMenu}
                             >
-                                {link.name}
-                            </button>
+                                <span
+                                    className="mr-4 mt-3 text-4xl font-semibold leading-none"
+                                >
+                                    {(index + 1).toString().padStart(2, "0")}
+                                </span>
+                                <span className="text-[128px] font-bold uppercase leading-none">
+                                    {link.name}
+                                </span>
+                            </motion.button>
                         ))}
                     </motion.div>
                 )}
