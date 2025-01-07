a2e35f6cbe98476bc95c8b81979c60135077b214
Matsuel <matsuel0308@gmail.com>
2025-01-07 09:14:05 +0100

    Hook usePercentage -> assign value as number

--- src/components/CircleProgress.tsx
+++ src/components/CircleProgress.tsx
@@ -6,7 +6,6 @@ import React from "react";
 
 const CircleProgress = () => {
     const { scrollPercentage } = useScrollPercentage();
-    const percentageNumber = parseInt(scrollPercentage, 10);
 
     const scrollToTop = () => {
         window.scrollTo({ top: 0, behavior: "smooth" });
@@ -17,7 +16,7 @@ const CircleProgress = () => {
         <div className="fixed right-8 bottom-8 flex flex-row items-center justify-center gap-2">
             <button
                 onClick={scrollToTop}
-                className={`flex flex-row items-center justify-center border-none outline-none transition-all duration-500 ease-in-out ${percentageNumber < 10
+                className={`flex flex-row items-center justify-center border-none outline-none transition-all duration-500 ease-in-out ${scrollPercentage < 10
                     ? "opacity-0 pointer-events-none"
                     : "opacity-100 pointer-events-auto"
                     }`}
@@ -28,11 +27,11 @@ const CircleProgress = () => {
                 <div
                     className="w-10 h-10 rounded-full flex justify-center items-center"
                     style={{
-                        background: `conic-gradient(#00E46C ${percentageNumber}%, #173B2A ${percentageNumber}% 100%)`,
+                        background: `conic-gradient(#00E46C ${scrollPercentage}%, #173B2A ${scrollPercentage}% 100%)`,
                     }}
                 >
                     <div className="w-8 h-8 bg-background rounded-full flex justify-center items-center">
-                        <span className="text-[#00E46C] text-xs font-bold">{percentageNumber}</span>
+                        <span className="text-[#00E46C] text-xs font-bold">{scrollPercentage}</span>
                     </div>
                 </div>
             </div>
--- src/hooks/usePercentage.tsx
+++ src/hooks/usePercentage.tsx
@@ -3,14 +3,14 @@
 import { useEffect, useState } from 'react';
 
 export const useScrollPercentage = () => {
-    const [scrollPercentage, setScrollPercentage] = useState('000.00');
+    const [scrollPercentage, setScrollPercentage] = useState(0)
 
     useEffect(() => {
         const handleScroll = () => {
             const scrollTop = window.scrollY;
             const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-            const scrolled = ((scrollTop / docHeight) * 100).toFixed(2).padStart(6, '0');
-            setScrollPercentage(scrolled);
+            const scrolled = (scrollTop / docHeight) * 100;
+            setScrollPercentage(Math.round(Number(scrolled.toFixed(2))));
         };
 
         handleScroll();
