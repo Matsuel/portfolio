40064c5e425b3fbbfb98a9ff9e159a2e4adbd666
Matsuel <matsuel0308@gmail.com>
2025-01-10 22:32:09 +0100

    Constantes -> Add school and job

---
+++ src/constantes/school.ts
@@ -0,0 +1,14 @@
+export const schools = [
+    {
+        title: "Ynov Bordeaux",
+        role: "Etudiant en développement web",
+        date: "2022 - 2027",
+        link: "https://www.ynov.com/",
+    },
+    {
+        title: "Lycée Elie Faure",
+        role: "Bac Général",
+        date: "2019 - 2022",
+        link: "https://www.lyc-eliefaure.fr/",
+    }
+]
