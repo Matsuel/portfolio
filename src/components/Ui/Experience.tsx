f8ab9395f8e748478a9ada7afbb2a7145cee39d2
Matsuel <matsuel0308@gmail.com>
2025-01-13 19:09:57 +0100

    Edit styles

--- src/components/Ui/Experience.tsx
+++ src/components/Ui/Experience.tsx
@@ -17,13 +17,13 @@ const Experience = ({
     image
 }: ExperienceProps) => {
     return (
-        <div className='w-4/5 h-auto relative text-text pl-8'>
+        <div className='w-4/5 h-auto relative text-text pl-8 flex flex-col gap-2'>
             <div className='w-8 h-8 absolute -top-4 -left-4 bg-white rounded-full'>
                 <Image src={image} alt={title} width={100} height={100} />
             </div>
-            <h3 className='text-2xl font-bold'>{title}</h3>
-            <p className='text-lg text-secondary'>{date}</p>
-            <p className='text-lg'>{role}</p>
+            <h3 className='text-3xl font-bold'>{title}</h3>
+            <p className='text-xl text-secondary'>{date}</p>
+            <p className='text-xl'>{role}</p>
         </div>
     )
 }
