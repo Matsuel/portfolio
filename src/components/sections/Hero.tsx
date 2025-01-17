462afba9635810ed9770d9ba5a25898104b54df5
Matsuel <matsuel0308@gmail.com>
2025-01-17 23:04:45 +0100

    Hero -> Rework section v1

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -8,27 +8,22 @@ const Hero = () => {
 
 
     return (
-        <section id='hero' className="w-full max-h-screen min-h-screen flex flex-col justify-center p-8 items-center relative overflow-hidden" >
+        <section id='hero' className="w-full max-h-screen min-h-screen flex flex-col justify-center p-8 gap-12 items-center relative overflow-hidden" >
             <Image className='absolute top-0 left-0 w-full h-full object-cover' src='/yacht.jpg' alt='yacht' width={1920} height={1080} />
 
-
-            <p className='z-10 w-[60%] text-4xl font-bold text-text text-center'>
+            <h1 className='z-10 text-[160px] leading-none font-extrabold text-text uppercase'>Matheo Lang</h1>
+            <p className='z-10 w-[65%] text-4xl font-bold text-text text-center mb-32'>
                 Développeur web spécialisé dans la création de solutions simples mais efficaces, avec une approche qui redéfinit les standards du design
             </p>
-
-            {/* <p className='absolute right-8 w-[25%] text-text text-2xl'>
-                "Pour atteindre un nouveau rivage, il faut accepter de quitter le port."
-                — Christophe Colomb
-            </p> */}
-
-            <div className='bottom-12 z-10 w-auto h-auto flex flex-col items-center'>
-                <h1 className='text-[160px] leading-none font-extrabold text-text uppercase'>Matheo Lang</h1>
-
-            </div>
 
             <Button variant='primary' className='mt-12 z-10' onClick={() => scrollToSection('contact')}>
                 Signaler votre intérêt
             </Button>
+
+            <p className='absolute bottom-8 text-text font-medium text-2xl'>
+                "Pour atteindre un nouveau rivage, il faut accepter de quitter le port."
+                — Christophe Colomb
+            </p>
         </section>
     )
 }
