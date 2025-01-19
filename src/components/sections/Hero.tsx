9cd7c80754e923b8388b49e9460756f6f3259cd6
Matsuel <matsuel0308@gmail.com>
2025-01-19 19:52:00 +0100

    Edit Hero wallpaper

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,10 +6,9 @@ import Image from 'next/image';
 
 const Hero = () => {
 
-
     return (
         <section id='hero' className="w-full max-h-screen min-h-screen flex flex-col justify-center p-8 gap-12 items-center relative overflow-hidden" >
-            <Image className='absolute top-0 left-0 w-full h-full object-cover' src='/yacht.jpg' alt='yacht' width={1920} height={1080} />
+            <Image className='absolute top-0 left-0 w-full h-full object-center' src='/nyc.jpg' alt='yacht' width={1920} height={1080} />
 
             <h1 className='z-10 text-[160px] leading-none font-extrabold text-text uppercase'>Matheo Lang</h1>
             <p className='z-10 w-[65%] text-4xl font-bold text-color text-center mb-32'>
@@ -19,11 +18,6 @@ const Hero = () => {
             <Button variant='primary' className='mt-12 z-10' onClick={() => scrollToSection('contact')}>
                 Signaler votre intérêt
             </Button>
-
-            <p className='absolute bottom-8 text-color font-medium text-2xl'>
-                "Pour atteindre un nouveau rivage, il faut accepter de quitter le port."
-                — Christophe Colomb
-            </p>
         </section>
     )
 }
