2695856bcb0d6e95c31f81a94897621dcc01d230
Matsuel <matsuel0308@gmail.com>
2025-01-20 21:36:51 +0100

    Hero -> Add SimpleArrow component and update Hero section layout and content

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,35 +4,38 @@ import Button from '@/components/Ui/Button'
 import { scrollToSection } from '@/utils/scroll';
 import Image from 'next/image';
 import Title from '../Ui/Title';
+import SimpleArrow from '@/assets/SimpleArrow';
 
 const Hero = () => {
 
     return (
-        <section id='hero' className="w-full max-h-screen h-screen flex flex-col justify-center p-8 gap-12 items-center relative overflow-hidden" >
+        <section id='hero' className="w-full max-h-screen h-screen flex flex-col justify-start p-8 gap-12 items-center relative overflow-hidden" >
             <Image
                 className="absolute top-0 left-0 w-full h-full object-cover"
-                src="/nyc.jpg"
+                src="/Paris.jpg"
                 alt="yacht"
-                width={1920}
-                height={1080}
-                priority
+                width={5235}
+                height={1745}
             />
 
-            <Title className='z-10 absolute top-24'>
-                Matheo Lang
-            </Title>
+            <div className='w-auto z-10 flex flex-col items-start gap-16'>
 
-            <p className='z-10 w-[65%] text-4xl font-bold text-color text-center'>
-                Développeur web spécialisé dans la création de solutions simples mais efficaces, avec une approche qui redéfinit les standards du design
-            </p>
+                <Title className='w-1/3 z-10'>
+                    Matheo Lang
+                </Title>
 
-            <Button variant='primary' className='z-10' onClick={() => scrollToSection('contact')}>
+                <p className='z-10 w-[47%] text-4xl font-bold text-color'>
+                    Développeur web spécialisé dans la création de solutions simples mais efficaces, avec une approche qui redéfinit les standards du design
+                    <Button variant='transparent' className='text-3xl font-bold' whileTap={1}>
+                        En savoir plus <SimpleArrow />
+                    </Button>
+                </p>
+
+            </div>
+
+            <Button variant='primary' className='absolute z-10 bottom-40' onClick={() => scrollToSection('contact')}>
                 Signaler votre intérêt
             </Button>
-
-            {/* <p className='z-10 w-[30%] absolute left-8 text-2xl font-semibold text-color text-center mt-12'>
-                "Ceux qui osent imaginer peuvent construire ce que d'autres pensent impossible." — Elon Musk
-            </p> */}
         </section>
     )
 }
