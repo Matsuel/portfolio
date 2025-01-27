6a5440cda6d6c7ef9bf0a87cd09191014938178e
Matsuel <matsuel0308@gmail.com>
2025-01-27 13:59:43 +0100

    Delete olds components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
-import CircleProgress from '@/components/Ui/CircleProgress';
-import Hour from '@/components/Ui/Hour';
 import Hero from '@/components/sections/Hero';
 import Loader from '@/components/Ui/Loader';
-import Projets from '@/components/sections/Projets';
 import Navbar from '@/components/Navbar';
 import MenuIcon from '@/components/Navbar/MenuIcon';
-import Parcours from '@/components/sections/Parcours';
-import Services from '@/components/sections/Services';
-import Contact from '@/components/sections/Contact';
 
 export default function Home() {
 
@@ -17,13 +11,8 @@ export default function Home() {
       <MenuIcon />
       <Navbar />
       {/* <CircleProgress /> */}
-      <Hour />
       <Hero />
-      {/* <Loader />
-      <Projets />
-      <Parcours />
-      <Services />
-      <Contact /> */}
+      {/* <Loader /> */}
     </div>
   );
 }
