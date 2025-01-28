e06a551df9e79db0f1f21e4b42c58702424b8ca1
Matsuel <matsuel0308@gmail.com>
2025-01-28 12:11:35 +0100

    Section Projets -> Ajout assets dot, Projet

---
+++ src/components/Ui/Projet.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { motion } from "framer-motion";
+import Image from 'next/image';
+import Link from 'next/link';
+import Git from '@/assets/Git';
+
+interface ProjetProps {
+    index: number;
+    active: number;
+    title: string;
+    description: string;
+    github: string;
+    link: string;
+    technologies: string[];
+    icon: string;
+    projectsLength: number;
+}
+
+const Projet = ({
+    index,
+    active,
+    title,
+    description,
+    github,
+    link,
+    technologies,
+    icon,
+    projectsLength
+}: ProjetProps) => {
+
+    const isActive = index === active;
+    const isNext = (index === (active + 1) % projectsLength);
+
+    return (
+        <motion.div
+            key={title}
+            className="absolute flex flex-col items-start justify-start gap-6 rounded-lg bg-carousel p-8"
+            initial={{ opacity: 0, scale: 0.8, x: "100%" }}
+            animate={{
+                zIndex: isActive ? 2 : 1,
+                opacity: isActive || isNext ? 1 : 0,
+                x: isActive ? "0%" : isNext ? "120%" : "-100%",
+                scale: isActive ? 1 : 0.85,
+                width: isActive ? "35%" : "30%",
+                height: isActive ? "90%" : "80%",
+            }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+        >
+            <div className='w-auto h-auto flex flex-row items-center gap-4'>
+                <Link href={link} target='_blank' className='w-10 h-10 flex items-center justify-center rounded-full'>
+                    <Image src={icon} alt={title} width={40} height={40} />
+                </Link>
+                <Link href={github} target='_blank'>
+                    <Git size={30} />
+                </Link>
+            </div>
+            <h4 className="text-2xl font-medium text-coloreven">{title}</h4>
+            <p className="text-lg font-normal text-coloreven">{description}</p>
+        </motion.div>
+    )
+}
+
+export default Projet
---
+++ src/constantes/projects.ts
@@ -0,0 +1,27 @@
+
+export const projects = [
+    {
+        title: "CACBO-Badminton",
+        description: "Site web pour le club de badminton Carbon-Blanais, basé à Carbon-Blanc. Ce site permet de présenter le club, les horaires, les tarifs, et même de s'inscrire en ligne.",
+        link: "https://cacbobad.fr/",
+        github: "https://github.com/Matsuel/CACBO-Badminton",
+        technologies: ["Next.js", "TailwindCSS", "TypeScript", "Figma"],
+        icon: "/cacbo.png",
+    },
+    {
+        title: "ATPEscrime",
+        description: "Ce site internet a été réalisé dans le but de montrer les actions de l'association à travers la pratique de l'escrime pour dénoncer les différents types de violences dans notre société.",
+        link: "https://atpescrime.com",
+        github: "https://github.com/Matsuel/ATPE",
+        technologies: ["Next.js", "SCSS", "TypeScript"],
+        icon: "/atpe.png",
+    },
+    {
+        title: "ONU",
+        description: "Projet Next.js réalisé en 3ème année chez Ynov Bordeaux, avec une équipe de 3 personnes. Ce jeu est un clone du jeu UNO, mais en version en ligne.",
+        link: "https://onu.alexandrebel.me/",
+        github: "https://github.com/Matsuel/ONU",
+        technologies: ["Next.js", "TypeScript", "Socket.io", "TailwindCSS"],
+        icon: "/onu.png",
+    }
+]
