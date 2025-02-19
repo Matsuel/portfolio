5a831fbe53398ef8d7880542ebb0ed49287c840d
Matsuel <matsuel0308@gmail.com>
2025-02-19 13:33:21 +0100

    Hero -> update content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,14 @@ import Hero from '@/components/sections/Hero';
 import Loader from '@/components/Ui/Loader';
 import Navbar from '@/components/Navbar';
 import MenuIcon from '@/components/Navbar/MenuIcon';
-import HomeButton from '@/components/HomeButton';
-import Projets from '@/components/sections/Projets';
-import Parcours from '@/components/sections/Parcours';
 
 export default function Home() {
 
   return (
     <div className="w-full bg-background flex flex-col h-auto">
-      <HomeButton />
       <MenuIcon />
       <Navbar />
       <Hero />
-      <Projets />
-      <Parcours />
       <Loader />
     </div>
   );
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,49 +1,18 @@
 "use client";
 import React from 'react'
-import Button from '@/components/Ui/Button'
-import { scrollToSection } from '@/utils/scroll';
 import Image from 'next/image';
-import Title from '../Ui/Title';
 
 const Hero = () => {
 
     return (
-        <section id='hero' className="w-full max-h-screen h-screen flex flex-col justify-start p-24 pt-32 items-center relative overflow-hidden" >
+        <section id='hero' className="w-full max-h-screen h-screen flex flex-col justify-start px-24 pt-32 items-center relative overflow-hidden" >
             <Image
                 className="absolute top-0 left-0 w-full h-full object-cover"
-                src="/Paris.jpg"
+                src="/bordeaux.jpg"
                 alt="yacht"
                 width={5235}
                 height={1745}
             />
-
-            <div className='w-full z-10 flex flex-col items-start gap-12'>
-
-                <Title className='w-1/3'>
-                    Matheo Lang
-                </Title>
-
-                <p className='w-[30%] text-2xl font-bold text-color'>
-                    Votre projet mérite une vitrine exceptionnelle.
-                    Mon objectif est de concevoir une solution à la hauteur de vos ambitions -
-                    <Button
-                        className='p-0 ml-3 font-bold text-2xl'
-                        variant='transparent'
-                    >
-                        En savoir plus
-                    </Button>
-                </p>
-
-            </div>
-
-            <Button
-                className='absolute z-10 flex flex-row items-center justify-center bottom-32'
-                onClick={() => scrollToSection('contact')}
-                whileTap={1}
-                variant='primary'
-            >
-                Construisons ensemble votre succès <Image src='/rightArrow.svg' alt='right arrow' width={24} height={24} />
-            </Button>
         </section>
     )
 }
