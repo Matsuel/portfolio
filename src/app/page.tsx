ff73c2f1446e53c4dc36b21bd3594734d751fa41
Matsuel <matsuel0308@gmail.com>
2025-02-19 15:29:45 +0100

    Add socials links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Loader from '@/components/Ui/Loader';
 import Navbar from '@/components/Navbar';
 import NavbarMenu from '@/components/Navbar/NavbarMenu';
 import LangSelector from '@/components/Ui/LangSelector';
+import Socials from '@/components/Ui/Socials';
 
 export default function Home() {
 
   return (
     <div className="w-full bg-background flex flex-col h-auto">
       <LangSelector />
+      <Socials />
       <NavbarMenu />
       <Navbar />
       <Hero />
---
+++ src/constantes/socials.ts
@@ -0,0 +1,5 @@
+export const socials = [
+    { icon: '/github.svg', link: 'https://github.com/Matsuel' },
+    { icon: '/linkedin.svg', link: 'https://www.linkedin.com/in/matheo-lang' },
+    { icon: '/instagram.svg', link: 'https://www.instagram.com/matheo_lang/' },
+]
