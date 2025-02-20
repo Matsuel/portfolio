c9ef0404821b5b40bf62b24d89d9b8fd11768087
Matsuel <matsuel0308@gmail.com>
2025-02-20 15:20:00 +0100

    Services -> Add animation on hover text

--- src/components/Ui/Service.tsx
+++ src/components/Ui/Service.tsx
@@ -1,3 +1,4 @@
+import Arrow from '@/assets/Arrow'
 import Image from 'next/image'
 import React from 'react'
 
@@ -16,15 +17,15 @@ const Service = ({
     const expandedClass = expanded ? 'w-[49%]' : 'w-[32%]'
 
     return (
-        <div className={`${expandedClass} h-96 relative bg-white text-black p-8 rounded-xl flex flex-col gap-6`}>
-            <h4 className='text-2xl font-semibold'>{title}</h4>
+        <div className={`${expandedClass} h-96 relative bg-[#F7F7F7] text-black p-8 rounded-xl flex flex-col gap-6`}>
+            <h4 className='text-3xl font-semibold'>{title}</h4>
 
             <p className='text-xl text-[#505A63]'>
                 {description}
             </p>
 
-            <button className='absolute bottom-8 right-8 cursor-pointer'>
-                <Image src='/arrow.svg' width={40} height={40} alt='arrow' />
+            <button className='absolute bottom-8 right-8 cursor-pointer rounded-full hover:bg-black hover:text-white transition-all duration-300'>
+                <Arrow size={40} />
             </button>
         </div>
     )
