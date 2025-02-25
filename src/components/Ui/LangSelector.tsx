79f3a3c4c9d949eaad5159fdaa877266af690ad2
Matsuel <matsuel0308@gmail.com>
2025-02-25 21:43:45 +0100

    Rename ba to eus , create all dict

--- src/components/Ui/LangSelector.tsx
+++ src/components/Ui/LangSelector.tsx
@@ -6,7 +6,7 @@ const LangSelector = () => {
         <div className='h-8 fixed z-20 flex flex-row justify-center items-center gap-1 bg-background rounded-full px-2 top-8 right-24'>
             <Lang flagSrc='/fr.svg' lang='fr' />
             <Lang flagSrc='/en.svg' lang='en' />
-            <Lang flagSrc='/ba.svg' lang='ba' />
+            <Lang flagSrc='/eus.svg' lang='eus' />
 
         </div>
     )
