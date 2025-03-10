2b971ae8918f747d070b63c015904ef15569f396
Matsuel <matsuel0308@gmail.com>
2025-03-10 10:40:54 +0100

    Navbar -> Patch Button errors

--- src/components/Navbar/NavbarIcon.tsx
+++ src/components/Navbar/NavbarIcon.tsx
@@ -15,7 +15,7 @@ const NavbarIcon = ({
 
 
     return (
-        <button
+        <div
             className="bg-transparent border-none cursor-pointer flex p-0"
             aria-label="Main Menu"
         >
@@ -48,7 +48,7 @@ const NavbarIcon = ({
                     d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
                 />
             </svg>
-        </button>
+        </div>
     );
 };
 
--- src/components/Navbar/NavbarMenu.tsx
+++ src/components/Navbar/NavbarMenu.tsx
@@ -10,12 +10,12 @@ const NavbarMenu = () => {
     const t = useTranslations('Navbar')
 
     return (
-        <div className='h-8 fixed z-50 flex flex-row justify-center items-center gap-1 bg-background rounded-full px-3 cursor-pointer bottom-8 left-[50%] transform translate-x-[-50%]' onClick={toggleMenu}>
+        <button className='h-8 fixed z-50 flex flex-row justify-center items-center gap-1 bg-background rounded-full px-3 cursor-pointer bottom-8 left-[50%] transform translate-x-[-50%]' onClick={toggleMenu}>
             <NavbarIcon size={20} />
             <h1 className='text-white text-base uppercase font-extrabold'>
                 {t('button')}
             </h1>
-        </div>
+        </button>
     )
 }
 
