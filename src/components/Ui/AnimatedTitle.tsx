f5ec9a3b78ac0f1dd2ae17fc4814202306747374
Matsuel <matsuel0308@gmail.com>
2025-03-15 00:02:22 +0100

    Section Hero -> Start responsive

--- src/components/Ui/AnimatedTitle.tsx
+++ src/components/Ui/AnimatedTitle.tsx
@@ -10,7 +10,7 @@ const AnimatedTitle = ({
     title
 }: AnimatedTitleProps) => {
     return (
-        <motion.h1 className="text-9xl text-white font-extrabold uppercase"
+        <motion.h1 className="xl:text-9xl sm:text-8xl text-6xl text-white font-extrabold uppercase"
             initial={{ opacity: 0, y: 100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 1, delay: 2.7 }}
