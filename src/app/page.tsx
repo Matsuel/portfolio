6966820cd4de8ea7eb27e8c0f41edee01cc69c5c
Matsuel <matheolang@icloud.com>
2025-05-12 16:02:37 +0200

    feat: add Hero component with background image and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-start">
-      <h1 className="text-7xl font-semibold">
+    <div className="w-full min-h-screen flex items-start bg-black">
+      {/* <h1 className="text-7xl font-semibold">
         Hello World!
-      </h1>
+      </h1> */}
+
+      <Hero />
+
+
       
     </div>
   );
---
+++ src/components/Hero.tsx
@@ -0,0 +1,20 @@
+"use client";
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div
+            className='w-full h-screen flex items-start relative'
+            style={{
+                backgroundImage: "url('/background.webp')",
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+            }}
+        >
+
+        </div>
+    )
+}
+
+export default Hero
