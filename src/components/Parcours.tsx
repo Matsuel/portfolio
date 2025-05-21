287023fc984f94a66c7c31aaad20d09cf4a2433f
Matsuel <matheolang@icloud.com>
2025-05-21 21:27:08 +0200

    add todo

--- src/components/Parcours.tsx
+++ src/components/Parcours.tsx
@@ -6,6 +6,11 @@ const Parcours = () => {
         <SectionLayout
             id='parcours'
         >
+            Faire une sorte de multisteper avec un logo loading sur l'expérience tant qu'elle n'est pas active
+            Regarder GSAP pour les animations
+            L'expérience active est placée au centre de l'écran
+            et celle qui charge en dessous
+            les autres sont cachées
 
         </SectionLayout>
     )
