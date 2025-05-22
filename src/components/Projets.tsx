065e0cf470ccdf46477e77bb75811d7331ee24d6
Matsuel <matheolang@icloud.com>
2025-05-22 21:31:02 +0200

    feat: ajouter le composant Projets avec une structure de carte pour les animations

---
+++ src/components/Projets.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import SectionLayout from './SectionLayout'
+
+const Projets = () => {
+    return (
+        <SectionLayout
+            id='projets'
+            >
+                Faire une sorte de carte qui s'empile lors du scroll
+                Regarder GSAP pour les animations
+            </SectionLayout>
+    )
+}
+
+export default Projets
