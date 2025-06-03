3c98444074e06a9658ff73227b1c1e6a4823d35c
Matsuel <matheolang@icloud.com>
2025-06-03 21:45:27 +0200

    feat: supprimer la hauteur automatique du composant Services pour un meilleur alignement

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -7,7 +7,7 @@ const Services = () => {
     return (
         <SectionLayout
             id='services'
-            className='h-auto flex flex-col items-center justify-center'
+            className='flex flex-col items-center justify-center'
         >
             <BentoGrid items={services} />
 
