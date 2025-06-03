28bb4fb4d3f12ff2eef95ee41d32a5191b4a244e
Matsuel <matheolang@icloud.com>
2025-06-03 21:36:13 +0200

    feat: mettre à jour les icônes des services dans le fichier services.tsx

--- constants/services.tsx
+++ constants/services.tsx
@@ -1,9 +1,10 @@
 import { BentoItem } from "@/types";
 import {
-    CheckCircle,
     TrendingUp,
-    Video,
     Globe,
+    Unplug,
+    Hammer,
+    HeartPulse
 } from "lucide-react";
 
 export const services: BentoItem[] = [
@@ -19,28 +20,28 @@ export const services: BentoItem[] = [
     {
         title: "Développement Backend",
         description: "Construisez un backend robuste et scalable avec les dernières technologies (Node.js, Python, Go). Parfait pour gérer vos données, votre sécurité, et vos flux d’informations.",
-        icon: <CheckCircle className="w-4 h-4 text-emerald-500" />,
+        icon: <Hammer className="w-4 h-4 text-emerald-500" />,
         status: "Updated",
         tags: ["Node.js", "Python", "Go", "API"],
     },
     {
         title: "Web App Full-Stack",
         description: "Je développe des applications web interactives et performantes en utilisant des technologies modernes. Mon objectif est de créer des solutions robustes et évolutives qui répondent parfaitement à vos besoins spécifiques. Je prends en charge toutes les étapes du développement, de la conception à la mise en production, en mettant l’accent sur la qualité du code et la scalabilité des applications.",
-        icon: <Video className="w-4 h-4 text-purple-500" />,
+        icon: <Globe className="w-4 h-4 text-purple-500" />,
         status: "Active",
         tags: ["React", "Node.js", "Full-Stack", "Web App"],
     },
     {
         title: "Maintenance et support technique",
         description: "Je propose des services de maintenance réguliers pour vos sites et applications web afin de garantir qu'ils restent à jour, fonctionnels et sécurisés. Que ce soit pour des mises à jour de contenu, des corrections de bugs, ou des ajustements techniques, je veille à ce que votre projet continue de fonctionner de manière optimale. Vous pourrez ainsi vous concentrer sur vos objectifs sans vous soucier des aspects techniques.",
-        icon: <Globe className="w-4 h-4 text-sky-500" />,
+        icon: <HeartPulse className="w-4 h-4 text-sky-500" />,
         status: "Beta",
         tags: ["Maintenance", "Support", "Infrastructure"],
     },
     {
         title: "Intégration de services tiers",
         description: "J'intègre des API externes et des services tiers dans vos applications pour étendre leurs fonctionnalités. Que vous ayez besoin d'une solution de paiement, d'un système de géolocalisation, ou d'une autre fonctionnalité externe, je m'assure que l'intégration soit fluide, rapide et sécurisée.",
-        icon: <Globe className="w-4 h-4 text-sky-500" />,
+        icon: <Unplug className="w-4 h-4 text-sky-500" />,
         status: "Beta",
         tags: ["Intégration", "API", "Services Tiers"],
     }
