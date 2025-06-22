54bb07cdfa54e2e03da6dfdb5dfad4df729c0a22
Matsuel <matheolang@icloud.com>
2025-06-22 13:37:44 +0200

    feat: ajouter des animations au composant Mentions Légales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,6 +24,5 @@ export default function Home() {
 // TODO: https://21st.dev/shadcnblockscom/faq-5/default (mentions légales)
 // TODO: https://vercel.com/legal/support-terms Mentions légales
 // TODO: Ajout Contact
-// TODO: Animations Mentions légales
 // TODO: Ajout titre et description dans le sectionLayout
 // TODO: Ajout Projets
