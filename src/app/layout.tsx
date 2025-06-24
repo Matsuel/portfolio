45a05d499e8c84345e56d39ed591acc772fef592
Matsuel <matheolang@icloud.com>
2025-06-24 12:55:29 +0200

    feat: ajouter des métadonnées et des règles de robots dans le composant RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { BASE_URL } from "./sitemap";
 
 const switzer = localFont({
   src: "../fonts/Switzer.ttf",
@@ -8,7 +9,41 @@ const switzer = localFont({
 
 export const metadata: Metadata = {
   title: "Mathéo Lang",
+  applicationName: "Mathéo Lang - Portfolio",
   description: "Portfolio de Mathéo Lang, développeur web",
+  keywords: ["Mathéo Lang", "portfolio", "développeur web", "web developer", "Bordeaux", "freelance"],
+  authors: [{ name: "Mathéo Lang", url: BASE_URL }],
+  creator: "Mathéo Lang",
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+    },
+  },
+  openGraph: {
+    title: "Mathéo Lang",
+    description: "Portfolio de Mathéo Lang, développeur web",
+    url: BASE_URL,
+    siteName: "Mathéo Lang",
+    images: [
+      {
+        url: `${BASE_URL}/favicon.ico`,
+        width: 1200,
+        height: 630,
+        alt: "Mathéo Lang - Portfolio de Mathéo Lang, développeur web",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+    countryName: "France",
+  },
+  icons: {
+    icon: `${BASE_URL}/favicon.ico`,
+    shortcut: `${BASE_URL}/favicon.ico`,
+    apple: `${BASE_URL}/favicon.ico`,
+  },
 };
 
 export default function RootLayout({
