28ae9651522e4283d4bc0e3f52bc215cdfbbff7b
Matsuel <matheolang@icloud.com>
2025-07-21 18:38:52 +0200

    fix: supprimer la configuration de redirection pour le domaine matheolang.fr

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        has: [
-          {
-            type: "host",
-            value: "matheolang.fr",
-            protocol: "http",
-          }
-        ],
-        destination: "https://www.matheolang.fr/:path*",
-        permanent: true,
-      }
-    ]
-  }
 };
 
 export default nextConfig;
