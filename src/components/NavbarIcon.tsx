9db96795229c14910cde7948647cb80359a486f2
Matsuel <matheolang@icloud.com>
2025-09-12 21:59:54 +0200

    fix: mettre à jour la couleur du texte dans le composant NavbarIcon

--- src/components/NavbarIcon.tsx
+++ src/components/NavbarIcon.tsx
@@ -14,7 +14,7 @@ const NavbarIcon = ({
         <Link
             href={link}
             target='_blank'
-            className='w-[50px] h-[50px] flex items-center justify-center p-3 rounded-full transition-all duration-300 text-foreground border border-glassmorphism-border backdrop-blur-glassmorphism bg-[linear-gradient(145deg,_rgba(255,255,255,0.1)_0%,_rgba(255,255,255,0)_100%)] hover:bg-[linear-gradient(145deg,_rgba(255,255,255,0.4)_0%,_rgba(255,255,255,0.1)_100%)] shadow-xl'
+            className='w-[50px] h-[50px] flex items-center justify-center p-3 rounded-full transition-all duration-300 text-[#F92C47] border border-glassmorphism-border backdrop-blur-glassmorphism bg-[linear-gradient(145deg,_rgba(255,255,255,0.1)_0%,_rgba(255,255,255,0)_100%)] hover:bg-[linear-gradient(145deg,_rgba(255,255,255,0.4)_0%,_rgba(255,255,255,0.1)_100%)] shadow-xl'
         >
             {icon}
         </Link>
