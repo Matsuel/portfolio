843641b5e2909805e62a915cefa4e89018efb92c
Matsuel <matheolang@icloud.com>
2025-09-16 21:28:10 +0200

    fix: ajuster la largeur de la barre de progression et réorganiser le rendu des éléments dans NavbarProgress

--- src/components/NavbarProgress.tsx
+++ src/components/NavbarProgress.tsx
@@ -12,11 +12,17 @@ const NavbarProgress = ({
 }: NavbarProgressProps) => {
     return (
         <div className='w-auto h-auto flex flex-col items-start justify-center gap-1'>
-            <div className='h-8 w-8 bg-black rounded-lg flex items-center justify-center'>
-                {navbarLinks.find(link => link.sectionId.replace("#", "") === activeId)?.icon}
+
+            <div className='flex flex-row gap-2 items-center'>
+                <div className='h-8 w-8 bg-black rounded-lg flex items-center justify-center'>
+                    {navbarLinks.find(link => link.sectionId.replace("#", "") === activeId)?.icon}
+                </div>
+                <span className='font-medium text-sm text-[#262626]'>
+                    {navbarLinks.find(link => link.sectionId.replace("#", "") === activeId)?.name}
+                </span>
             </div>
 
-            <div className='w-20 h-1 bg-[#ddd] rounded-full overflow-hidden'>
+            <div className='w-28 h-1 bg-[#ddd] rounded-full overflow-hidden'>
                 <div
                     className='h-full bg-[#262626] ease-in-out'
                     style={{ width: `${progress}%` }}
